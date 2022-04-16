572e09a9a22464892b72e4dbe885797484596fa1
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2022-04-16 16:22:22 -0300

    Cargado carpeta base de TC3

---
+++ Trabajos Python/TC3/TC3-EJ4.py
@@ -0,0 +1,26 @@
+#Autómatas y Gramáticas -Universidad de Mendoza-
+#Alumnos: Marcos Miglierina - Alexis Lino
+# Trabajo en Clase N°3 - Ejercicio N°4: (a|b)*(abb)
+
+def Ejercicio1(input):
+    estado = "A"
+
+    for i in range(len(input)):
+        caracter = input[i]
+
+        if (estado == "A"):
+            print("Estado A", "caracter:", caracter)
+            
+            
+        if (estado == "B"):
+            print("Estado B", "caracter:", caracter)
+
+            
+        if (estado == "C"):
+            print("Estado C", "caracter:", caracter)
+
+
+            
+
+valor = str(input("Ingresar input: "))
+Ejercicio1(input = valor)
